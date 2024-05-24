659b78de2c9f052d1eee4e48b1df9b890a21115b
Connor Lindsey <cblindsey3@gmail.com>
2024-05-24 15:14:44 -0700

    track page views in example app

--- examples/app-router-example/src/components/Layout.tsx
+++ examples/app-router-example/src/components/Layout.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Container } from './Container'
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 
 export function Layout({
   children,
@@ -9,6 +13,21 @@ export function Layout({
   children: React.ReactNode
   showFooter?: boolean
 }) {
+  const pathname = usePathname
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (window.bento !== undefined) {
+        // Note: if the user is identified or logged in, you can identify their page view by running.
+        // Example:
+        // if (user) {
+        //  window.bento.identify(email);
+        // }
+        window.bento.view()
+      }
+    }, 0)
+  }, [pathname])
+
   return (
     <>
       <Header />
